4313aa8d5ae2889436f206be6304e50baa1e18d4
gregor77kr <7837167@naver.com>
2022-06-09 19:48:02 +0900

    Add logger configuration

--- whiteboard/src/main/java/net/mwav/agora/whiteboard/config/WebMvcConfigurerImpl.java
+++ whiteboard/src/main/java/net/mwav/agora/whiteboard/config/WebMvcConfigurerImpl.java
@@ -1,9 +1,6 @@
 package net.mwav.agora.whiteboard.config;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,8 +9,6 @@ public class WebMvcConfigurerImpl implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**")
-				.addResourceLocations("classpath:/templates/", "classpath:/static/")
-				.setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/", "classpath:/static/");
 	}
 }
