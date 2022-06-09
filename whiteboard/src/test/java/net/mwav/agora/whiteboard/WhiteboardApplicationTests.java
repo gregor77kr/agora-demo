e278e27e25f0d8c808e563d6852a7842afb96d26
gregor77kr <7837167@naver.com>
2022-06-09 16:37:13 +0900

    First commit

---
+++ whiteboard/src/test/java/net/mwav/agora/whiteboard/WhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mwav.agora.whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
