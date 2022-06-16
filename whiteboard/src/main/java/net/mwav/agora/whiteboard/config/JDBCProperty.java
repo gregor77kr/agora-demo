473b289179e8a552cda787bd84239504f77be313
gregor77kr <7837167@naver.com>
2022-06-16 17:17:23 +0900

    Update JDBCProperty.java

--- whiteboard/src/main/java/net/mwav/agora/whiteboard/config/JDBCProperty.java
+++ whiteboard/src/main/java/net/mwav/agora/whiteboard/config/JDBCProperty.java
@@ -1,7 +1,6 @@
 package net.mwav.agora.whiteboard.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
