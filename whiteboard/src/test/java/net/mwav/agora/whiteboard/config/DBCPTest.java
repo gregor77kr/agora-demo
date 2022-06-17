8df33a73936e985890ee95a6df19e1b975479791
gregor77kr <7837167@naver.com>
2022-06-17 16:42:23 +0900

    Remove unused import

--- whiteboard/src/test/java/net/mwav/agora/whiteboard/config/DBCPTest.java
+++ whiteboard/src/test/java/net/mwav/agora/whiteboard/config/DBCPTest.java
@@ -12,8 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.zaxxer.hikari.HikariDataSource;
 
-import net.mwav.agora.whiteboard.config.DBCPConfig;
-
 @SpringBootTest
 class DBCPTest {
 
--- whiteboard/src/test/java/net/mwav/agora/whiteboard/config/PropertyTest.java
+++ whiteboard/src/test/java/net/mwav/agora/whiteboard/config/PropertyTest.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import net.mwav.agora.whiteboard.config.AgoraProperty;
-
 @SpringBootTest
 class PropertyTest {
 
