fb4b7d24107b5addda2fc4e57b799a137649a22c
gregor77kr <7837167@naver.com>
2022-06-18 12:57:45 +0900

    Remove unused

--- whiteboard/src/main/java/net/mwav/agora/whiteboard/security/entity/Authority.java
+++ whiteboard/src/main/java/net/mwav/agora/whiteboard/security/entity/Authority.java
@@ -1,14 +1,11 @@
 package net.mwav.agora.whiteboard.security.entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
-@Table(name = "Authority")
 public class Authority implements GrantedAuthority {
 
 	private static final long serialVersionUID = 5792321961413369652L;
@@ -16,10 +13,8 @@ public class Authority implements GrantedAuthority {
 	@Id
 	private String role;
 
-	@Column
 	private String roleName;
 
-	@Column
 	private String roleDesc;
 
 	@Override
@@ -32,28 +27,24 @@ public class Authority implements GrantedAuthority {
 		return role;
 	}
 
-	public void setRole(String role) {
-		this.role = role;
-	}
-
 	public String getRoleName() {
 		return roleName;
-	}
-
-	public void setRoleName(String roleName) {
-		this.roleName = roleName;
 	}
 
 	public String getRoleDesc() {
 		return roleDesc;
 	}
 
-	public void setRoleDesc(String roleDesc) {
-		this.roleDesc = roleDesc;
+	public void setRole(String role) {
+		this.role = role;
 	}
 
-	public void setAuthority(String role) {
-		this.role = role;
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public void setRoleDesc(String roleDesc) {
+		this.roleDesc = roleDesc;
 	}
 
 	@Override
