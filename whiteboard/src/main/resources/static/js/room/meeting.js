2db8c625ca41e588067960b4208007f372fcdc16
gregor77kr <7837167@naver.com>
2022-08-06 02:56:07 +0900

    Add basic button event

--- whiteboard/src/main/resources/static/js/room/meeting.js
+++ whiteboard/src/main/resources/static/js/room/meeting.js
@@ -8,7 +8,6 @@ var whiteWebSdk, room;
 			region: "sg"
 		});
 
-		/** 
 		room = whiteWebSdk.joinRoom({
 			'uuid': uuid,
 			'uid': 'gregor77kr',
@@ -19,21 +18,53 @@ var whiteWebSdk, room;
 		}).catch((error) => {
 			console.error(error);
 		});
-		*/
-
 	});
 
-	document.on('click', 'img', (event) => {
+	document.on('click', '#imgClicker, #imgSelector, #imgPencil, #imgText, #imgEraser, #imgArrow, #imgLaserPointer, #imgHand', (event) => {
+		const appliancename = event.target.dataset.appliancename;
+
+		if (appliancename) {
+			room.then(r => {
+				r.setMemberState({
+					currentApplianceName: appliancename,
+					shapeType: "pentagram",
+					strokeColor: [255, 182, 200],
+					strokeWidth: 12,
+					textSize: 40,
+				});
+				return r;
+			});
+		}
+	});
+
+	document.on('click', '#imgClear', (event) => {
+		room.then(r => {
+			r.cleanCurrentScene();
+			return r;
+		});
+	});
+
+	document.on('mouseover', '.tool-box-cell-subscript', (event) => {
 		console.log(event.target);
 	});
 
-	document.on('onmouseover', 'img', (event) => {
+	document.on('mouseout', '.tool-box-cell-subscript', (event) => {
 		console.log(event.target);
-		console.log(this);
 	});
-	
-	document.on('onmouseout', 'img', (event) => {
-		console.log(event.target);
-		console.log(this);
+
+	document.on('mouseover', 'img', (event) => {
+		const hover = event.target.dataset.hover;
+
+		if (hover) {
+			event.target.src = hover;
+		}
+	});
+
+	document.on('mouseout', 'img', (event) => {
+		const hout = event.target.dataset.hout;
+
+		if (hout) {
+			event.target.src = hout;
+		}
 	});
 })();
