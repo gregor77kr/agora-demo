f6d27ecf3df7be5a532c91dd37b8e67f062f0718
gregor77kr <7837167@naver.com>
2022-08-11 01:54:13 +0900

    Add color picker

--- whiteboard/src/main/resources/static/js/room/meeting.js
+++ whiteboard/src/main/resources/static/js/room/meeting.js
@@ -22,13 +22,18 @@ var whiteWebSdk, room;
 
 	document.on('click', '#imgClicker, #imgSelector, #imgPencil, #imgText, #imgEraser, #imgArrow, #imgLaserPointer, #imgHand', (event) => {
 		const appliancename = event.target.dataset.appliancename;
+		
+		const divColor = document.querySelector('#divColor');
+		let selectedColor = divColor.style.backgroundColor || 'rgb(255, 255, 255)';
+		
+		selectedColor = selectedColor.replace(/ /g, '').replace('rgb(', '').replace(')', '').split(',').map(s => Number(s));
 
 		if (appliancename) {
 			room.then(r => {
 				r.setMemberState({
 					currentApplianceName: appliancename,
 					shapeType: "pentagram",
-					strokeColor: [255, 182, 200],
+					strokeColor: selectedColor,
 					strokeWidth: 12,
 					textSize: 40,
 				});
@@ -57,10 +62,12 @@ var whiteWebSdk, room;
 			event.target.dataset.toggle = 'false';
 			event.target.src = hout;
 			divPallet.classList.add('ant-popover-hidden');
+			divPallet.style.pointerEvents = 'none';
 		} else {
 			event.target.dataset.toggle = 'true';
 			event.target.src = hover;
 			divPallet.classList.remove('ant-popover-hidden');
+			divPallet.style.pointerEvents = 'auto';
 		}
 
 		const absoulteLeft = clientRect.left + event.target.clientWidth + 5;
@@ -68,6 +75,28 @@ var whiteWebSdk, room;
 
 		divPallet.style.left = absoulteLeft + 'px';
 		divPallet.style.top = absoluteTop + 'px';
+	});
+
+	document.on('click', '.cell-color', (event) => {
+		const colors = document.querySelectorAll('.cell-mid-color');
+		colors.forEach((color, i) => {
+			color.style.borderColor = 'rgb(255, 255, 255)';
+		});
+
+		event.target.parentElement.style.borderColor = 'rgb(0, 0, 0)';
+
+		let selectedColor = event.target.style.backgroundColor || 'rgb(255, 255, 255)';
+		const divColor = document.querySelector('#divColor');
+		divColor.style.backgroundColor = selectedColor;
+		
+		selectedColor = selectedColor.replace(/ /g, '').replace('rgb(', '').replace(')', '').split(',').map(s => Number(s));
+
+		room.then(r => {
+			r.setMemberState({
+				strokeColor: selectedColor
+			});
+			return r;
+		});
 	});
 
 	document.on('click', '.tool-box-cell>img:not(.tool-box-cell-subscript)', (event) => {
