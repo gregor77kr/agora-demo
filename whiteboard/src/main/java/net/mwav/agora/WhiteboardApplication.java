f3ee20153a5f3969e3bc580616bba27af62f9b94
gregor77kr <7837167@naver.com>
2022-08-20 01:02:06 +0900

    Add dependency to war deploy

--- whiteboard/src/main/java/net/mwav/agora/WhiteboardApplication.java
+++ whiteboard/src/main/java/net/mwav/agora/WhiteboardApplication.java
@@ -2,9 +2,16 @@ package net.mwav.agora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WhiteboardApplication {
+public class WhiteboardApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WhiteboardApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhiteboardApplication.class, args);
